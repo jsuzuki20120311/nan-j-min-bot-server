3f059f6895f3799b60b2cc2fc0efcdfcf09f0b38
jsuzuki20120311 <shukatu.2012.5.25@gmail.com>
2019-03-15 00:33:39 +0900

    不要な処理削除

--- bot/shell-scripts/decoder.sh
+++ bot/shell-scripts/decoder.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd $(cd $(dirname $0); pwd)
-
 PROBLEM=my_problem
 
 MODEL=lstm_seq2seq_attention_bidirectional_encoder
@@ -15,5 +13,8 @@ TRAIN_DIR=$HOME/src/github.com/jsuzuki20120311/nan-j-min-bot-server/bot/t2t-trai
 
 DECODE_FILE=$1
 DECODE_TO_FILE=$2
+
+echo $1
+echo $2
 
 t2t-decoder --data_dir=$DATA_DIR --problem=$PROBLEM --model=$MODEL --hparams_set=$HPARAMS --output_dir=$TRAIN_DIR --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" --t2t_usr_dir=$USR_DIR  --decode_from_file=$DECODE_FILE  --decode_to_file=$DECODE_TO_FILE
