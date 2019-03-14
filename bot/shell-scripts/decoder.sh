140840ef5c56c9919287799a911959309bcbb34d
jsuzuki20120311 <shukatu.2012.5.25@gmail.com>
2019-03-14 17:59:56 +0900

    実行シェルの修正。

--- bot/shell-scripts/decoder.sh
+++ bot/shell-scripts/decoder.sh
@@ -2,6 +2,8 @@
 
 cd $(cd $(dirname $0); pwd)
 
+source activate nan-j-min-bot-amazonlinux2
+
 PROBLEM=my_problem
 
 MODEL=lstm_seq2seq_attention_bidirectional_encoder
@@ -16,4 +18,4 @@ TRAIN_DIR=$HOME/src/github.com/jsuzuki20120311/nan-j-min-bot-server/bot/t2t-trai
 DECODE_FILE=$1
 DECODE_TO_FILE=$2
 
-pipenv run t2t-decoder --data_dir=$DATA_DIR --problem=$PROBLEM --model=$MODEL --hparams_set=$HPARAMS --output_dir=$TRAIN_DIR --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" --t2t_usr_dir=$USR_DIR  --decode_from_file=$DECODE_FILE  --decode_to_file=$DECODE_TO_FILE
+t2t-decoder --data_dir=$DATA_DIR --problem=$PROBLEM --model=$MODEL --hparams_set=$HPARAMS --output_dir=$TRAIN_DIR --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" --t2t_usr_dir=$USR_DIR  --decode_from_file=$DECODE_FILE  --decode_to_file=$DECODE_TO_FILE
